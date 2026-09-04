e9797dda9d772daefe78c1d197470a74c34e8eec
CUR_LAB2 <CUR_LAB2@aline-Gogo.cslevel4.local>
2026-09-04 16:54:58 +0200

    deploy

--- build.sh
+++ build.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
+# exit on error
 set -o errexit
 
+# Install dependencies
+pip install -r requirements.txt
+
+# Run Django management commands
 python manage.py collectstatic --no-input
-python manage.py migrate --no-input
+python manage.py migrate
