cb5a15820c68c489de0b24cc9a4b87361a9d87e1
engineerblack95 <engineerblack95@gmail.com>
2026-09-22 05:19:33 -0700

    Fix static storage (no manifest)

--- config/settings.py
+++ config/settings.py
@@ -180,7 +180,8 @@ LOCALE_PATHS = [BASE_DIR / 'locale']
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_DIRS = [BASE_DIR / 'static']
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
+WHITENOISE_USE_FINDERS = True
 
 # ──────────────────────────────────────────────────────────────────────────────
 # Media files
